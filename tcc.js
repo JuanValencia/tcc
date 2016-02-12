1aa1f76685653194698547d027e2afdd21159079
Juan Valencia <isfree@gmail.com>
2016-02-11 16:33:55 -0800

    added admin

--- tcc.js
+++ tcc.js
@@ -1,19 +1,5 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
 }
 
 if (Meteor.isServer) {
@@ -21,3 +7,59 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Router.route('/');
+
+AdminConfig = {
+  name: 'Tech City Church',
+  adminEmails: ['isfree@gmail.com'],
+  collections: {
+    Posts: {}
+  }
+};
+
+Posts = new Meteor.Collection('posts');
+Schemas = {};
+
+Schemas.Posts = new SimpleSchema({
+  title: {
+    type: String,
+    max: 60
+  },
+  content: {
+    type: String,
+    autoform: {
+      rows: 5
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: 'Date',
+    autoValue: function () {
+      if (this.isInsert) {
+        return new Date();
+      }
+    }
+  },
+  owner: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    autoValue: function () {
+      if (this.isInsert) {
+        return Meteor.userId();
+      }
+    },
+    autoform: {
+      options: function () {
+        _.map(Meteor.users.find().fetch(), function (user) {
+          return {
+            label: user.emails[0].address,
+            value: user._id
+          };
+        });
+      }
+    }
+  }
+});
+
+Posts.attachSchema(Schemas.Posts)
